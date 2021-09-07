0692ecc4d3edfb0f212755bb682a51a9170e5b9f
mohammed-shees <49620014+mohammed-shees@users.noreply.github.com>
2021-09-07 22:36:29 +0530

    Add files via upload

---
+++ areaofcircle.c
@@ -0,0 +1,16 @@
+#include "areacalculator.h"
+
+
+
+
+
+/**
+ * @brief  function to find squares of a number
+ * 
+ * @param a operand1
+ * @return int  it returns the output value
+ */
+ int Areaofcircle(int a);
+{
+   return 3.14*a*a;
+}
